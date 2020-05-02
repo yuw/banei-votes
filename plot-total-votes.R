3435f48ab7615be3419a1d7824b7a7a96ed49c7a
Yuwsuke Kieda <p.typog@gmail.com>
2020-05-03 01:56:21 +0900

    update

--- plot-total-votes.R
+++ plot-total-votes.R
@@ -1,9 +1,19 @@
+#
+# Rscript banei-votes-2020.csv $round
+# $round = 通算開催数
+#
+
 data <- read.csv("banei-votes-2020.csv", header=T)
 #data[,1] <- as.Date(data[,1], "%Y-%m-%d")
 
 races <- NULL
 
-for (i in 4:0){
+round <- as.integer(commandArgs(trailingOnly=TRUE)[1])# 引数としてとった値を整数に変換
+round <- round - 1
+
+n <- round:0
+
+for (i in n){
     races <- cbind(races, data[(11 * i + 1) : (11 + 11 * i), 3])
 }
 
@@ -16,4 +26,5 @@ xaxt="n",
 xlab="race #", ylab="total votes")
 axis(side=1,at=1:11)
 
-matpoints(data[1:11,2], data[45:55,3], type="l", lwd=6, col="white")
+matpoints(data[1:11,2], data[(11 * round + 1) : (11 + 11 * round), 3],
+type="l", lwd=6, col="white")
