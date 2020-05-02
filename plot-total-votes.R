d8c9479755e8cbaa7314dfbfa0a49c0ecb916e76
Yuwsuke Kieda <p.typog@gmail.com>
2020-05-03 00:14:54 +0900

    update

--- plot-total-votes.R
+++ plot-total-votes.R
@@ -9,18 +9,17 @@ data[12:22,3],
 data[1:11,3])
 
 par(bg="black", fg="white",
-col.lab="white", cex.lab=1.5, col.axis="white")
+col.axis="white", col.lab="white", cex.lab=1.5)
 
-pdf("plot.pdf",
-bg="black", fg="white")
+#pdf("plot.pdf", bg="black", fg="white")
 
 matplot(data[1:11,2], races,
 type="l", lwd=3,
 xaxt="n",
 xlab="race #", ylab="total votes",
-col.lab="white", cex.lab=1.5, col.axis="white")
+col.axis="white", col.lab="white", cex.lab=1.5)
 axis(side=1,at=1:11)
 
 matpoints(data[1:11,2], data[45:55,3], type="l", lwd=6, col="white")
 
-dev.off()
+#dev.off()
